6781cfa5fd3906dc7a043ac42bbf53f4bc40e326
Nick Tyler <tylern@canisius.edu>
2016-02-10 13:23:30 -0500

    Changed tab for spaces

--- getwebpage.py
+++ getwebpage.py
@@ -4,23 +4,23 @@ from datetime import datetime
 import pandas as pd
 
 def getwebpage(url):
-	session = dryscrape.Session()
-	session.visit(url)
-	response = session.body()
-	return BeautifulSoup(response, "lxml")
+    session = dryscrape.Session()
+    session.visit(url)
+    response = session.body()
+    return BeautifulSoup(response, "lxml")
 
 def get_counts(username,soups):
-	counts = {}
-	for link in soups.find_all('a'): 
-		link = str(link)
-		beginning = link.find('username')
-		if beginning != -1:
-			name = link[beginning+9:]
-			counts['time'] = datetime.now()
-			if name.find(username) != -1:
-				name = name[:-4]
-				state = name[name.find("state=")+6:name.find(">")-1]
-				count = name[name.find(">")+1:]
-				counts[state] = int(count)
-	counts = pd.DataFrame([counts])
-	return counts
+    counts = {}
+    for link in soups.find_all('a'): 
+        link = str(link)
+        beginning = link.find('username')
+        if beginning != -1:
+            name = link[beginning+9:]
+            counts['time'] = datetime.now()
+            if name.find(username) != -1:
+                name = name[:-4]
+                state = name[name.find("state=")+6:name.find(">")-1]
+                count = name[name.find(">")+1:]
+                counts[state] = int(count)
+    counts = pd.DataFrame([counts])
+    return counts
--- watchthefarm.py
+++ watchthefarm.py
@@ -7,25 +7,25 @@ import pandas as pd
 
 parser = OptionParser()
 parser.add_option("-u", help="jlab username", 
-				dest="username",default='')
+                dest="username",default='')
 
 parser.add_option("-t", "--tape",
                 action="store_false", dest="url_type", default=True,
                 help="Gets the tape jobs instead of batch jobs")
 parser.add_option("-j","--json",
-				action="store_true",dest="json_data", default=False,
-				help="Save data to JSON format")
+                action="store_true",dest="json_data", default=False,
+                help="Save data to JSON format")
 
 (options, args) = parser.parse_args()
 
 if options.url_type:
-	url = 'http://scicomp.jlab.org/scicomp/#/auger/jobs'
+    url = 'http://scicomp.jlab.org/scicomp/#/auger/jobs'
 else:
-	url = 'http://scicomp.jlab.org/scicomp/#/jasmine/jobs'
+    url = 'http://scicomp.jlab.org/scicomp/#/jasmine/jobs'
 
 data = get_counts(options.username,getwebpage(url))
 
 if options.json_data:
-	data.to_json('data.json')
+    data.to_json('data.json')
 else:
-	print data.to_string()
+    print data.to_string()
